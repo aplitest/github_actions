aaf2edd59446a7ec6d3db5f2ffe062a527896093
aplitest <64444468+aplitest@users.noreply.github.com>
2024-09-06 01:45:50 +0100

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['master'],
+  branches: ['master', {name:"stage",prerelease:true}],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
